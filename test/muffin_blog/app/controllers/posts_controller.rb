8d3a1f98d519f030f4ab00ba1171f2651fc882cb
Mike West <michael.west@phishme.com>
2019-07-24 15:52:35 -0600

    Minimal middleware and routing setup

--- test/muffin_blog/app/controllers/posts_controller.rb
+++ test/muffin_blog/app/controllers/posts_controller.rb
@@ -1,8 +1,16 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
+  def new
+  end
+
   def index
     @posts = Post.all
+
+    response.write "<h1>The Muffin Blog</h1>"
+    @posts.each do |post|
+      response.write "<h2>#{post.title}<h2>"
+    end
   end
 
   def show
